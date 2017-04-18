4a2636e3735978248b9993a05f71970abb976e5d
Rohan Rk <rohanrk2009@gmail.com>
2017-04-18 15:21:49 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Stress Relief
+
+In an ideal world every time you're stressed a cute cat would appear to help you with stress. This is the next best thing. Tap the button for a picture of a cat :D
--- StressRelief/ViewController.swift
+++ StressRelief/ViewController.swift
@@ -10,9 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var stressButton: UIButton!
+    private var images: [UIImage]?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.view.backgroundColor = UIColor(patternImage: UIImage(named: "PurpleSky")!)
+        
+        let imageName = "cuteCatBubble"
+        let image = UIImage(named: imageName)
+        let imageView = UIImageView(image: image!)
+        
+        imageView.frame = CGRect(x: 5, y: 500, width: 400, height: 262)
+        //view.addConstraint(NSLayoutConstraint(item: imageView, attribute: NSLayoutAttribute.leading, relatedBy: <#T##NSLayoutRelation#>, toItem: <#T##Any?#>, attribute: <#T##NSLayoutAttribute#>, multiplier: <#T##CGFloat#>, constant: <#T##CGFloat#>))
+        view.addSubview(imageView)
+        
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +35,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func popCat(_ sender: Any) {
+        var rand = arc4random_uniform(<#T##__upper_bound: UInt32##UInt32#>)
+    }
 
 }
 
